c13158ca03d1d52f670f72ce22b34bfd51864725
Muhamad Faishal Irawan <muhammadfaishal193@gmail.com>
2024-03-10 12:50:59 +0700

    chore: add comments and ref link and cleanup

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -6,11 +6,12 @@ declare global {
   }
 }
 
-chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (request) => {
   if (request.type === "INJECT_SCRIPT") {
     chrome.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id as number },
+        // readding event listener, ref:https://greasyfork.org/en/scripts/477710-quicker-backloggd-log/code
         func: () => window.onmount(),
         world: "MAIN",
       });
